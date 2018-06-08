5d1dbdbd1529396f8f154be6fb8c411e6a7ad27d
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-06-08 17:37:33 +0200

    Agent

--- src/app/services/agent/agent.service.ts
+++ src/app/services/agent/agent.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Agent } from 'http';
 import { Observable } from 'rxjs/Observable';
+import { Agent } from '../../agent';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
